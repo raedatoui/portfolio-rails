a1945b8b2f132b806ed16a33963382fdfb08f151
Raed Atoui <raed.atoui@gmail.com>
2017-06-22 03:20:21 -0400

    pwd

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Portfolio
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.i18n.enforce_available_locales =  false
+    config.i18n.enforce_available_locales = false
   end
 end
